b17c72bd963561e5e3afe045aae889ce199d06ac
Deniskin0110 <110915190+Deniskin0110@users.noreply.github.com>
2022-08-10 20:04:14 +0800

    Add files via upload

---
+++ Deniskin.java
@@ -0,0 +1,128 @@
+package calcRA;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Deniskin {
+    static Scanner scanner = new Scanner(System.in);
+    static int number1, number2;
+    static char operation;
+    static int result;
+
+    public static void main (String[] args) {
+        System.out.println("Введите выражение");
+        String userInput = scanner.nextLine();
+        char[] under_char = new char[10];
+        for (int i = 0; i < userInput.length(); i++) {
+            under_char[i] = userInput.charAt(i);
+            if (under_char[i] == '+') {
+                operation = '+';
+            }
+            if (under_char[i] == '-') {
+                operation = '-';
+            }
+            if (under_char[i] == '*') {
+                operation = '*';
+            }
+            if (under_char[i] == '/') {
+                operation = '/';
+            }
+        }
+        String under_charString = String.valueOf(under_char);
+        String[] blacks = under_charString.split("[+-/*]");
+        String stable00 = blacks[0];
+        String stable01 = blacks[1];
+        String string03 = stable01.trim();
+        number1 = denToNumber(stable00);
+        number2 = denToNumber(string03);
+        if ( number1 < 0 && number2 < 0 ) {
+            result = 0;
+        } else {
+            result = calculated(number1, number2, operation);
+                String resultRoman = convertNumToDen(result);
+            if (result > 0) {
+                System.out.println(resultRoman);
+            } else {
+                System.out.println("Не верное выражение!");
+            }
+        }
+        number1 = Integer.parseInt(stable00);
+        number2 = Integer.parseInt(string03);
+        if (number1 <= 10 && number2 <= 10 ) {
+            result = calculated(number1, number2, operation);
+               System.out.println(result);
+        }
+    }
+
+    private static String convertNumToDen(int numArabian) {
+        String[] den = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+        };
+        return den[numArabian];
+    }
+
+
+    private static int denToNumber(String Deniskin) {
+        try {
+            switch (Deniskin) {
+                case "I":
+                    return 1;
+                case "II":
+                    return 2;
+                case "III":
+                    return 3;
+                case "IV":
+                    return 4;
+                case "V":
+                    return 5;
+                case "VI":
+                    return 6;
+                case "VII":
+                    return 7;
+                case "VIII":
+                    return 8;
+                case "IX":
+                    return 9;
+                case "X":
+                    return 10;
+            }
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Неверный формат данных");
+        }
+        return -1;
+    }
+
+    public static int calculated (int num1, int num2, char op) {
+        int result = 0;
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                try {
+                    result = num1 / num2;
+                } catch (ArithmeticException | InputMismatchException e) {
+
+                    System.out.println("Делить на 0 нельзя!");
+
+                    break;
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Не верный знак операции");
+        }
+        return result;
+    }
+}
